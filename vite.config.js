d42415d0e6974e1b07e46ab24256757d51a2e976
Yehonatan <y.yomtov8@gmail.com>
2024-12-29 11:14:20 +0200

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+   plugins: [react()],
+   build: {
+      outDir: "dist",
+      assetsDir: "_assets", // Output static assets to a separate folder for easier caching
+      manifest: true, // Generates a manifest.json for better file management
+   },
+   server: {
+      port: 3000, // Optional, if you need to test locally
+   },
+});
